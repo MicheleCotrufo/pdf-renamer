7e854893d589d2026d9c1ddbd685ad7c3e352cf4
Michele Cotrufo <64863140+MicheleCotrufo@users.noreply.github.com>
2021-04-23 23:44:24 -0400

    Add project files.

---
+++ pdf-renamer/__init__.py
@@ -0,0 +1,4 @@
+from .pdf2doi import pdf2doi
+from .finders import *
+from .bibtex_makers import *
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
+setuptools.setup(name='pdf-renamer',
+      version='0.1',
+      description='A python library/command-line tool to automatically rename the pdf files of scientific publications.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/MicheleCotrufo/pdf-renamer',
+      author='Michele Cotrufo',
+      author_email='michele.cotrufo@gmail.com',
+      license='MIT',
+      #entry_points = {
+      #  'console_scripts': ["pdf2doi = pdf2doi.pdf2doi:main"],
+      #},
+      packages=['pdf-renamer'],
+      install_requires= required_packages,
+      zip_safe=False)
