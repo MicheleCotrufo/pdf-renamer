2dcae0bddca96414677b2aa09ef5340bd6c907da
Michele Cotrufo <64863140+MicheleCotrufo@users.noreply.github.com>
2021-11-16 23:42:33 -0500

    change the config.py structure

--- pdfrenamer/__init__.py
+++ pdfrenamer/__init__.py
@@ -1,30 +1,23 @@
 import logging
 
+import logging
+
 # Setup logging
-
-#Change the formatter for the logger of the pdf2doi library; add a prefix '\t' in front of all
-#output created by pdf2doi
-logger = logging.getLogger("pdf2doi")
-logger.setLevel(level=logging.INFO)
-logger.handlers =[]
-if not logger.handlers:
-    formatter = logging.Formatter("\t[pdf2doi]: %(message)s")
-    ch = logging.StreamHandler()
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-logger.propagate = False
-#####
-
 logger = logging.getLogger("pdf-renamer")
 logger.setLevel(level=logging.INFO)
 if not logger.handlers:
-    formatter = logging.Formatter("[pdf-renamer] %(message)s")
+    formatter = logging.Formatter("[pdf-renamer]: %(message)s")
     ch = logging.StreamHandler()
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 logger.propagate = False
 
-from .config import Config
+from .config import config
+config.ReadParamsINIfile()
+
+config.set('verbose',config.get('verbose')) #This is a quick and dirty way (to improve in the future) to make sure that the verbosity of the pdf2doi logger is properly set according
+                                            #to the current value of config.get('verbose') (see config.py file for details)
+
 from .main import rename,build_filename
 from .filename_creators import *
 
